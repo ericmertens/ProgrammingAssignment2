0e37b450a9696464e20dbab098294e727fc986da
Eric Mertens <Eric@Erics-MacBook-Pro.local>
2015-08-18 10:37:11 +0200

    first working draft

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,14 @@
 ## Author : Eric Mertens
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## This file contains 2 functions : makeCacheMatrix and cacheSolve
+## Those 2 functions are coming from the example given for the Programming Assignement 2
+
+## The function makeCacheMatrix is an adaptation of the makeVector given as example 
+## for the Programming Assignment 2
+## "m" (for mean) has been renamed to "im" (for inverted matrix)
+## "mean" has been replace by "solve"
+## "setmean" and "getmean" have been renamed to "setinvmat" and "getinvmat"
+
 
 makeCacheMatrix <- function(x = matrix()) {
         im <- NULL
@@ -18,7 +24,11 @@ makeCacheMatrix <- function(x = matrix()) {
              getinvmat = getinvmat)
 }
 
-## Write a short comment describing this function
+## The function cacheSolve is an adaptation of the cacheMean given as example 
+## for the Programming Assignment 2
+## "m" (for mean) has been renamed to "im" (for inverted matrix)
+## "mean" has been replace by "solve"
+## "setmean" and "getmean" have been renamed to "setinvmat" and "getinvmat"
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -33,4 +43,12 @@ cacheSolve <- function(x, ...) {
         im
 }
 
-## Use c=rbind(c(1, -1/4), c(-1/4, 1)) as test matrix
+## Use the following code as test if needed:
+## c <- rbind(c(1, -1/4), c(-1/4, 1))
+## cm <- makeCacheMatrix(c)
+## c %*% cacheSolve(cm)
+## the last line should return an identity matrix (2X2)
+##     [,1] [,2]
+##[1,]    1    0
+##[2,]    0    1
+##
